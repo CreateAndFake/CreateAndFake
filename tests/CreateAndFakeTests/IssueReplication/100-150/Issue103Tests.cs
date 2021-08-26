9c784fa5d95fad3d020143a99784ea988666bf97
Werebunny <18252929+Werebunny@users.noreply.github.com>
2021-08-26 00:44:48 -0600

    Fix text assert error messages. Add create variant extension.

---
+++ tests/CreateAndFakeTests/IssueReplication/100-150/Issue103Tests.cs
@@ -0,0 +1,18 @@
+using CreateAndFake;
+using CreateAndFake.Fluent;
+using CreateAndFake.Toolbox.AsserterTool;
+using Xunit;
+
+namespace CreateAndFakeTests.IssueReplication
+{
+    /// <summary>Verifies issue is resolved.</summary>
+    public static class Issue103Tests
+    {
+        [Theory, RandomData]
+        internal static void Issue103_AssertFailIncludesTestValue(string text)
+        {
+            string alt = Tools.Mutator.Variant(text);
+            Tools.Asserter.Throws<AssertException>(() => text.Assert().Contains(alt)).Message.Assert().Contains(alt);
+        }
+    }
+}
